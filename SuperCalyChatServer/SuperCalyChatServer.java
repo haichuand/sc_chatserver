346685e1ca503ce30e1d24902e2a3947abc2eded
anuagg <anuagg0102@gmail.com>
2016-10-16 22:36:18 -0700

    Attempt to add http server functionalities in chat server

--- SuperCalyChatServer/SuperCalyChatServer.java
+++ SuperCalyChatServer/SuperCalyChatServer.java
@@ -7,11 +7,20 @@ package SuperCalyChatServer;
 
 import SuperCalyChatServer.DAO.SuperDao;
 import static SuperCalyChatServer.SmackCcsClient.logger;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.logging.Level;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
 import org.json.simple.parser.ParseException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -19,10 +28,13 @@ import org.json.simple.parser.ParseException;
  */
 public class SuperCalyChatServer {
     
-    public static void main(String[] args) {
+    private static ServerSocket socket;
+    public static int PORT = 9000;
+
+    public static void main(String[] args) throws IOException {
         final String projectId = "1076145801492";
         final String password = "AIzaSyA_B4SmgfKpMbSp32jJbWrCqQ6YxGAhoMo";
-
+	
         //connect();
         SmackCcsClient ccsClient = SmackCcsClient.prepareClient(projectId, password, true);
 
@@ -53,14 +65,92 @@ public class SuperCalyChatServer {
             e.printStackTrace();
         }
         
-        while(true){
-            try{
+        try
+        {
+            socket = new ServerSocket(PORT);
+            System.out.println("Waiting for clients requests....\n");
+            while(true){
                Thread.sleep(50);
-            }
-            catch(Exception e) {
-                e.printStackTrace();
+               Socket client = socket.accept();
+               proccessRequest(client);
             }
         }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        finally
+        {
+            socket.close();
+        }
+        
+        
     }
+
+    private static void proccessRequest(Socket client) {
+        String str;
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            str = reader.readLine();
+            if(str != null)
+            {
+                if(str.trim().contains("HTTP"))
+                {
+                    sendResponse(client);
+                }
+            }
+        }
+        catch(IOException e)
+        {
+            
+        }
+    }
+
+    private static void sendResponse(Socket client) throws IOException {
+         
+        String statusCode = "200";
+        String reasonPhrase = "OK";
+        HashMap<String,String> headers = new HashMap<>(); 
+        String message = "<p>Chat Server Running fine!</p>";
+        String version = "HTTP/1.1";
+        String response = "";
+ 
+        headers.put("Date", DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneId.of("GMT"))));
+        headers.put("Server", "ChatServer");
+        headers.put("Content-Type","text/html");
+        headers.put("Content-Length", Integer.toString(message.getBytes().length));
+        
+        response += version;
+        response += " " + statusCode;
+        response += " " + reasonPhrase;
+        response += "\r\n";
+        if(headers != null)
+        {
+            for(Map.Entry<String, String> entry : headers.entrySet())
+            {
+                response += entry.getKey()+ ":" + entry.getValue() + "\r\n";
+            }
+        }
+        response += "\r\n";
+        if(message != null)
+        {
+            response += message;
+        }
+        
+        try
+        {
+        client.getOutputStream().write(response.getBytes());
+        client.getOutputStream().flush();
+        }
+        catch(IOException e)
+        {
+             e.printStackTrace();
+        }
+        finally
+        {
+            client.close();
+        }
+    }
+    
     
 }
